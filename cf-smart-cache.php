6965b33b855ec9c33c3597a9138032e7e719a7d1
LoveDoLove <v0130p5100cuboss@gmail.com>
2025-07-05 13:18:21 +0800

    Initial commit

---
+++ cf-smart-cache.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:       Cloudflare Smart Cache
+ * Plugin Slug:       cf-smart-cache
+ * Plugin URI:        https://github.com/LoveDoLove/cloudflare-smart-cache
+ * Description:       Powerful all-in-one Cloudflare cache solution: edge HTML caching, automatic purging on post/category changes, and advanced admin controls for WordPress.
+ * Version:           1.0.0
+ * Author:            LoveDoLove
+ * Author URI:        https://github.com/LoveDoLove
+ * License:           MIT
+ * License URI:       https://opensource.org/licenses/MIT
+ * Text Domain:       cf-smart-cache
+ * Domain Path:       /languages
+ */
